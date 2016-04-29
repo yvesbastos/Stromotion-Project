f480bc7fb86fd925d04eac8792c00146b1f9bb9d
yvesbastos <raphael.bastos@me.com>
2016-04-29 15:58:18 -0300

    Added OpenCV lib and tests

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
 
 public class Main {
 
@@ -6,6 +10,16 @@ public class Main {
         FileSelectorPanel mainFrame = new FileSelectorPanel("mp4");
         mainFrame.pack();
         mainFrame.setVisible(true);
+        
+        openCVExample();
+        
+	}
+	
+	//TODO simple example to check if OpenCV is running on all machines. Delete afterwards.
+	private static void openCVExample() {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+        System.out.println("mat = " + mat.dump());
 	}
 
 }
